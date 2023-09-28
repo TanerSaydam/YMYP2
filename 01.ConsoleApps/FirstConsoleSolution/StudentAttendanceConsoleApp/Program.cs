5dbce3d209c5cbefe534545072535c231c878fe0
Taner Saydam <taner_and_you@hotmail.com>
2023-09-28 20:30:43 +0300

    .

--- 01.ConsoleApps/FirstConsoleSolution/StudentAttendanceConsoleApp/Program.cs
+++ 01.ConsoleApps/FirstConsoleSolution/StudentAttendanceConsoleApp/Program.cs
@@ -4,12 +4,55 @@ internal class Program
 {
     static void Main(string[] args)
     {
+        //string[] students = new string[4]; //array
+        List<Student> students = new()
+        {
+            new Student()
+            {
+                No = 1,
+                Name = "Taner Saydam",
+                Class = "2A",
+                DateOfBirth = Convert.ToDateTime("03.09.1989")
+            },
+            new Student()
+            {
+                No = 2,
+                Name = "Ahmet Yalın",
+                Class = "2A",
+                DateOfBirth = Convert.ToDateTime("01.01.1988")
+            }
+        };
+
+
+        var student = new Student()
+        {
+            No = 3,
+            Name = "Mehmet Can Ünaldı",
+            Class = "2A",
+            DateOfBirth = Convert.ToDateTime("01.02.1989")
+        };
+
+        students.Add(student);
+
+
         Console.WriteLine("Hi, this is my studens!");
-        Console.WriteLine("01. Taner");
-        Console.WriteLine("02. Ahmet");
-        Console.WriteLine("03. Mehmet");
+        foreach(var s in students)
+        {
+            Console.WriteLine($@"
+Number: {s.No}
+Name: {s.Name}
+Class: {s.Class},
+Date Of Birth: {s.DateOfBirth}
+------------------");
+        }
         Console.WriteLine("Tell me who is in class?");
         string result = Console.ReadLine();
+
+
+
+
+
+
         //int kontrolü ile sıfıtaki öğrencinin numarasını isteyin
         //int değer girilmezse hata fırlatın
         //değer int ise sınıftaki öğrenci numaralarında bu değeri arayın ve eğer böyle numaralı bir öğrenci yoksa hata fırlatın
@@ -18,4 +61,14 @@ internal class Program
         //Bonus ödev: Eğer öğrenci zaten sınıfta işaretlendiyse o zaman tekrar işretlemesin ve hata versin
         //Bonus ödev: tüm öğrenciler sınıfta olarak işaretlenirse uygulama tüm öğrenciler sınıfta desin ve kapansın
     }
+}
+
+
+//access modifier => erişim belirleyicisi
+internal class Student
+{
+    public int No { get; set; }
+    public string Name { get; set; } //property, TitleCase
+    public string Class { get; set; }
+    public DateTime DateOfBirth { get; set; }
 }
